eab0bd1fc7e2afa54012b5d2908e71cd5ddd9bec
mandeep127 <singhmandeep2@seasia.in>
2024-07-02 18:54:56 +0530

    update files

--- src/adminStore/authApi/authApiServices.js
+++ src/adminStore/authApi/authApiServices.js
@@ -1,6 +1,4 @@
-
-
-import axios from 'axios';
+import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
@@ -9,7 +7,7 @@ export const authLoginApi = async (data) => {
     const response = await axios.post(`${baseUrl}/admin/login`, data);
     return response.data; // assuming your API returns data in a usable format
   } catch (error) {
-    console.log('Error in authLoginApi:', error);
+    console.log("Error in authLoginApi:", error);
     throw error; // rethrow the error to be handled by the caller
   }
 };
