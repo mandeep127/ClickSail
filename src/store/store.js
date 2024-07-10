1c828f79d04e6da4c1bb28a47f707ec3cc5c85e5
SinghAkashdeep1 <singhakashdeep1@seasiaconnect.com>
2024-07-10 18:34:54 +0530

    store update

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,32 @@
+
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./authAPI/authApiSlice";
 import categoriesReducer from "./homeAPI/homeApiSlice";
 import productsReducer from "./productAPI/productApiSlice";
+import authReducer from "../adminStore/authApi/authApiSlice";
+import userReducer from "../adminStore/userApi/userApiSlice";
+import categoryReducer from "../adminStore/categoriesApi/categoriesApiSlices";
+import subcategoryReducer from "../adminStore/subCategoriesApi/subcategoriesApiSlice";
+import productReducer from "../adminStore/productApi/productApiSlice";
+import salesReducer from "../adminStore/salesApi/salesApiSlice";
+import dashboardReducer from "../adminStore/dashboardApi/dashboardApiSlice";
 
 export const store = configureStore({
   reducer: {
-    users: loginReducer,
-    categories: categoriesReducer,
-    products: productsReducer,
+    //front-end
+    
+      users: loginReducer,
+      categories: categoriesReducer,
+      products: productsReducer,
+    
+//back-end
+    auth: authReducer,
+    user :userReducer,
+    category : categoryReducer,
+    subcategory : subcategoryReducer,
+    product : productReducer,
+    sales : salesReducer,
+    dashboard : dashboardReducer
   },
 });
+
